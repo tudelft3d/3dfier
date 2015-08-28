f52d3df4dfe14daebc9d113c95b14e0f84e8506b
Hugo Ledoux <h.ledoux@tudelft.nl>
2015-08-28 14:44:36 +0200

    Polygon3d.cpp + Polygon.h added

---
+++ Polygon3d.cpp
@@ -0,0 +1,30 @@
+//
+//  Polygon3d.cpp
+//  boo
+//
+//  Created by Hugo Ledoux on 28/08/15.
+//  Copyright (c) 2015 Hugo Ledoux. All rights reserved.
+//
+
+#include "Polygon3d.h"
+
+Polygon3d::Polygon3d(Polygon2d* p, int type, std::string id) {
+  _id = id;
+  _p2d = p;
+  _type = type;
+}
+
+Polygon3d::~Polygon3d() {
+  // TODO: clear memory properly
+}
+
+Box Polygon3d::get_bbox2d() {
+  return bg::return_envelope<Box>(*_p2d);
+}
+
+
+std::string Polygon3d::get_id() {
+  return _id;
+}
+
+
---
+++ Polygon3d.h
@@ -0,0 +1,32 @@
+//
+//  Polygon3d.h
+//  boo
+//
+//  Created by Hugo Ledoux on 28/08/15.
+//  Copyright (c) 2015 Hugo Ledoux. All rights reserved.
+//
+
+#ifndef __3DFIER__Polygon3D__
+#define __3DFIER__Polygon3D__
+
+#include "definitions.h"
+
+
+class Polygon3d
+{
+public:
+  Polygon3d  ();
+  Polygon3d  (Polygon2d* p, int type, std::string id);
+  ~Polygon3d ();
+
+  Box get_bbox2d();
+  std::string get_id();
+     
+private:
+  Polygon2d*    _p2d;
+  std::string   _id;
+  int           _type;
+};
+
+
+#endif /* defined(__boo__Polygon3D__) */
