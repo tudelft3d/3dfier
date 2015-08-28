9ff73ad56e8b9bf637238d2bced4b2d188c45abb
Hugo Ledoux <h.ledoux@tudelft.nl>
2015-08-28 20:00:42 +0200

    doesn't work yet

--- Polygon3d.cpp
+++ Polygon3d.cpp
@@ -1,11 +1,9 @@
 
 #include "Polygon3d.h"
 
-Polygon3d::Polygon3d(Polygon2d* p, ExtrusionType type, std::string id) {
+Polygon3d::Polygon3d(Polygon2d* p, std::string id) {
   _id = id;
   _p2d = p;
-  _extrusiontype = type;
-  _total_elevation = 0.0;
   _no_points = 0;
 }
 
@@ -18,34 +16,6 @@ Box Polygon3d::get_bbox2d() {
   return bg::return_envelope<Box>(*_p2d);
 }
 
-double Polygon3d::get_elevation() {
-  if (_no_points == 0)
-    return -999;
-  else
-    return (_total_elevation / _no_points);
-}
-
-
-bool Polygon3d::add_elevation_point(double x, double y, double z) {
-  switch(_extrusiontype) {
-    case SIMPLE_AVG :
-      _no_points++;
-      _total_elevation += z;
-      break;       
-    case SIMPLE_MEDIAN :
-      _no_points++;
-      _medianvalues.push_back(z);
-      break;    
-    case TIN_ALL_POINTS :
-      _lsPts3d.push_back(new Point3d(x, y, z));
-      break;   
-    case TIN_SIMPLIFIED :
-      // TODO : what are the rules to simplify actually? start with just skipping points I'd say
-      break;   
-  }
-  return true;
-}
-
 std::string Polygon3d::get_id() {
   return _id;
 }
@@ -53,4 +23,30 @@ std::string Polygon3d::get_id() {
 Polygon2d* Polygon3d::get_polygon2d() {
     return _p2d;
 }
+
+//-------------------------------
+
+
+Polygon3d_SA::Polygon3d_SA(Polygon2d* p, std::string id) {
+  _id = id;
+  _p2d = p;
+  _no_points = 0;
+  _total_elevation = 0.0;
+}
+
+
+double Polygon3d_SA::get_elevation() {
+  if (_no_points == 0)
+    return -999;
+  else
+    return (_total_elevation / _no_points);
+}
+
+
+bool Polygon3d_SA::add_elevation_point(double x, double y, double z) {
+  _no_points++;
+  _total_elevation += z;
+  return true;
+}
+
 
--- Polygon3d.h
+++ Polygon3d.h
@@ -8,29 +8,36 @@
 class Polygon3d
 {
 public:
-  Polygon3d  ();
-  Polygon3d  (Polygon2d* p, ExtrusionType type, std::string id);
+  Polygon3d  (Polygon2d* p, std::string id);
   ~Polygon3d ();
+  virtual bool  add_elevation_point(double x, double y, double z) = 0;
+  virtual double      get_elevation() = 0;
+  std::string   get_id();
+  Polygon2d*    get_polygon2d();
+  Box           get_bbox2d();
 
-  bool        add_elevation_point(double x, double y, double z);
-
-  double      get_elevation();
-
-  Box         get_bbox2d();
-  std::string get_id();
-  Polygon2d*  get_polygon2d();
-     
-private:
+protected:
   Polygon2d*      _p2d;
   std::string     _id;
-  ExtrusionType   _extrusiontype;
   int             _no_points;
 
   // TODO : these should be put in classes that inherits from Polygon3d
-  double           _total_elevation; //-- SIMPLE_AVG method
   vector<double>   _medianvalues; //-- SIMPLE_MEDIAN method
   vector<Point3d*> _lsPts3d;
 };
+
+
+
+class Polygon3d_SA : public Polygon3d 
+{
+public:
+  Polygon3d_SA (Polygon2d* p, std::string id);
+  bool        add_elevation_point(double x, double y, double z);
+  double      get_elevation();
+private:
+  double      _total_elevation; 
+};
+
 
 
 #endif /* defined(__boo__Polygon3D__) */
