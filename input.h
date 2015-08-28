19ac6b7e2d8d14d0362c5a17e9e8e87e65c3edea
Hugo Ledoux <h.ledoux@tudelft.nl>
2015-08-28 17:18:14 +0200

    removed functions from input (went into Map3d)

--- input.h
+++ input.h
@@ -5,6 +5,5 @@
 #include "definitions.h"
 #include "Polygon3d.h"
 
-bool read_gml_file(std::string ifile, std::vector<std::string>& layers, std::string idfield, std::vector<Polygon3d*>& lsPolys);
 
 #endif
