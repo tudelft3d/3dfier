30ae376c13bf40a72dc5b82cb16418a55648212b
Hugo Ledoux <h.ledoux@tudelft.nl>
2016-05-10 17:09:42 +0200

    laszip cmake w/o the version crap

--- cmake/Modules/FindLASzip.cmake
+++ cmake/Modules/FindLASzip.cmake
@@ -53,48 +53,4 @@ FIND_LIBRARY(LASZIP_LIBRARY
   /usr/local/lib
   ${LASZIP_ROOT_DIR}/lib)
 
-IF(LASZIP_FOUND)
-  SET(LASZIP_LIBRARIES ${LASZIP_LIBRARY})
-ENDIF()
 
-IF(LASZIP_INCLUDE_DIR)
-  SET(LASZIP_VERSION 0)
-
-  SET(LASZIP_VERSION_H "${LASZIP_INCLUDE_DIR}/laszip/laszip.hpp")
-  FILE(READ ${LASZIP_VERSION_H} LASZIP_VERSION_H_CONTENTS)
-
-  IF (DEFINED LASZIP_VERSION_H_CONTENTS)
-    string(REGEX REPLACE ".*#define[ \t]LASZIP_VERSION_MAJOR[ \t]+([0-9]+).*" "\\1" LASZIP_VERSION_MAJOR "${LASZIP_VERSION_H_CONTENTS}")
-    string(REGEX REPLACE ".*#define[ \t]LASZIP_VERSION_MINOR[ \t]+([0-9]+).*" "\\1" LASZIP_VERSION_MINOR "${LASZIP_VERSION_H_CONTENTS}")
-    string(REGEX REPLACE ".*#define[ \t]LASZIP_VERSION_REVISION[ \t]+([0-9]+).*"   "\\1" LASZIP_VERSION_REVISION   "${LASZIP_VERSION_H_CONTENTS}")
-
-    if(NOT ${LASZIP_VERSION_MAJOR} MATCHES "[0-9]+")
-      message(FATAL_ERROR "LASzip version parsing failed for LASZIP_VERSION_MAJOR!")
-    endif()
-    if(NOT ${LASZIP_VERSION_MINOR} MATCHES "[0-9]+")
-      message(FATAL_ERROR "LASzip version parsing failed for LASZIP_VERSION_MINOR!")
-    endif()
-    if(NOT ${LASZIP_VERSION_REVISION} MATCHES "[0-9]+")
-      message(FATAL_ERROR "LASzip version parsing failed for LASZIP_VERSION_REVISION!")
-    endif()
-
-
-    SET(LASZIP_VERSION "${LASZIP_VERSION_MAJOR}.${LASZIP_VERSION_MINOR}.${LASZIP_VERSION_REVISION}"
-      CACHE INTERNAL "The version string for LASzip library")
-
-    IF (LASZIP_VERSION VERSION_EQUAL LASzip_FIND_VERSION OR
-        LASZIP_VERSION VERSION_GREATER LASzip_FIND_VERSION)
-      MESSAGE(STATUS "Found LASzip version: ${LASZIP_VERSION}")
-    ELSE()
-      MESSAGE(FATAL_ERROR "LASzip version check failed. Version ${LASZIP_VERSION} was found, at least version ${LASzip_FIND_VERSION} is required")
-    ENDIF()
-  ELSE()
-    MESSAGE(FATAL_ERROR "Failed to open ${LASZIP_VERSION_H} file")
-  ENDIF()
-
-ENDIF()
-
-# Handle the QUIETLY and REQUIRED arguments and set LASZIP_FOUND to TRUE
-# if all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LASzip DEFAULT_MSG LASZIP_LIBRARY LASZIP_INCLUDE_DIR)
