f072ae7f0e51c5a50c44a40f2117e872037dc202
Tom Commandeur <t.j.f.commandeur@tudelft.nl>
2016-08-30 15:12:36 +0200

    Roads only take LAS_GROUND class points and Forest uses a buffer of vertex radius for adding lidarpts

--- Forest.cpp
+++ Forest.cpp
@@ -47,7 +47,7 @@ bool Forest::add_elevation_point(double x, double y, double z, float radius, LAS
   }
   if (toadd && lastreturn && lasclass != LAS_BUILDING) {
     Point2 p(x, y);
-    if(bg::within(p, *(_p2))) {
+    if(bg::within(p, *(_p2)) && (this->get_distance_to_boundaries(p) > radius) ) {
       _lidarpts.push_back(Point3(x, y, z));
     }
     assign_elevation_to_vertex(x, y, z, radius);
