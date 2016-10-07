d852eb26df377da5c69256b44b774177fc31c18b
Hugo Ledoux <h.ledoux@tudelft.nl>
2016-10-07 14:40:23 +0200

    a few fix

--- geomtools.h
+++ geomtools.h
@@ -37,7 +37,7 @@ void get_point_inside(Ring2& ring, Point2& p);
 bool triangle_contains_segment(Triangle t, int a, int b);
 bool getCDT(const Polygon2* pgn,
             const std::vector< std::vector<int> > &z, 
-            std::set<Point3> &vertices, 
+            std::vector<Point3> &vertices, 
             std::vector<Triangle> &triangles, 
             const std::vector<Point3> &lidarpts = std::vector<Point3>());
 
