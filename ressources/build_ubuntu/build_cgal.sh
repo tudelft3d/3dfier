653efc89d075a31e1807ddfc069f015b91a909a4
Maarten Pronk <git@evetion.nl>
2017-09-12 10:51:09 +0200

    Fixed boost dep and archive typo.

--- ressources/build_ubuntu/build_cgal.sh
+++ ressources/build_ubuntu/build_cgal.sh
@@ -22,7 +22,7 @@ wget https://github.com/CGAL/cgal/archive/releases/CGAL-4.10.tar.gz
 wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.10/md5sum.txt
 md5sum -c md5sum.txt
 rm md5sum.txt
-tar -xvf laszip-src-2.2.0.tar.gz
+tar -xvf CGAL-4.10.tar.gz
 cd cgal-releases-CGAL-4.10
 
 mkdir build
