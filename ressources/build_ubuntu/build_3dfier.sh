c03f6b7045a57a0e48a2bfd50f29cd44bb573309
Maarten Pronk <git@evetion.nl>
2017-09-12 10:44:33 +0200

    Added latest cgal build.

--- ressources/build_ubuntu/build_3dfier.sh
+++ ressources/build_ubuntu/build_3dfier.sh
@@ -3,9 +3,9 @@
 cd $1
 # download the current release
 # not that probably you'll need to update the link to the newest release
-wget https://github.com/tudelft3d/3dfier/archive/v0.9.6.tar.gz
-tar -xvf v0.9.6.tar.gz
-cd 3dfier-0.9.6
+wget https://github.com/tudelft3d/3dfier/archive/v0.9.7.tar.gz
+tar -xvf v0.9.7.tar.gz
+cd 3dfier-0.9.7
 mkdir build
 cd build
 
@@ -15,14 +15,17 @@ cmake .. \
 -DLIBLAS_INCLUDE_DIR=$1/libLAS-1.8.1/build/include \
 -DLIBLAS_LIBRARY=$1/libLAS-1.8.1/build/lib/liblas.so \
 -DLASZIP_INCLUDE_DIR=$1/laszip-src-2.2.0/build/include \
--DLASZIP_LIBRARY=$1/laszip-src-2.2.0/build/lib/liblaszip.so
+-DLASZIP_LIBRARY=$1/laszip-src-2.2.0/build/lib/liblaszip.so \
+-DCGAL_DIR=$1/cgal-releases-CGAL-4.10/build
+
 
 cmake .. \
 -DLIBLASC_LIBRARY=$1/libLAS-1.8.1/build/lib/liblas_c.so \
 -DLIBLAS_INCLUDE_DIR=$1/libLAS-1.8.1/build/include \
 -DLIBLAS_LIBRARY=$1/libLAS-1.8.1/build/lib/liblas.so \
 -DLASZIP_INCLUDE_DIR=$1/laszip-src-2.2.0/build/include \
--DLASZIP_LIBRARY=$1/laszip-src-2.2.0/build/lib/liblaszip.so
+-DLASZIP_LIBRARY=$1/laszip-src-2.2.0/build/lib/liblaszip.so \
+-DCGAL_DIR=$1/cgal-releases-CGAL-4.10/build
 make
 
 #------
---
+++ ressources/build_ubuntu/build_cgal.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install script for 3dfier on Ubuntu 16.04 LTS
+# Parameters:
+#   $1 – path to Source Directory where LASzip, libLAS, 3dfier will be installed, e.g. /opt
+# Usage:
+#   ./build_ubuntu.sh /opt
+
+# You need to have write permission on the Source Directory
+# The script downloads the source files of LASzip, libLAS and 3dfier. You might
+# need to update the link to the latest 3dfier below.
+
+# Install CGAL, GDAL, yaml-cpp, boost on your own.
+# If you have the ubuntugis-(un)stable PPA added to your repositories, you can easily
+# install these with sudo apt install ...
+# Or you can use e.g. Synaptic Package Manager to the same.
+# you'll need libgdal, libcgal, libboost, libyaml-cpp0.5
+
+# CGAL
+cd $1
+wget https://github.com/CGAL/cgal/archive/releases/CGAL-4.10.tar.gz
+wget https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.10/md5sum.txt
+md5sum -c md5sum.txt
+rm md5sum.txt
+tar -xvf laszip-src-2.2.0.tar.gz
+cd cgal-releases-CGAL-4.10
+
+mkdir build
+cd build
+cmake ..
+make
