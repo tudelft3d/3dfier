d6ddd893ba7868d7a06b8ee0ea7db301cbad9258
Hugo Ledoux <h.ledoux@tudelft.nl>
2017-10-02 16:31:04 +0200

    added metadata about contact point in cityjson

--- Map3d.cpp
+++ Map3d.cpp
@@ -155,6 +155,7 @@ bool Map3d::get_cityjson(std::string filename) {
   j["version"] = "http://www.cityjson.org/version/0.2";
   j["metadata"] = {};
   j["metadata"]["datasetTitle"] = "my 3dfied map";
+  j["metadata"]["pointOfContact"] = "https://3d.bk.tudelft.nl";
   double b[] = {bg::get<bg::min_corner, 0>(_bbox),
                 bg::get<bg::min_corner, 1>(_bbox), 
                 0,
@@ -162,7 +163,6 @@ bool Map3d::get_cityjson(std::string filename) {
                 bg::get<bg::max_corner, 1>(_bbox), 
                 0};
   j["metadata"]["bbox"] = b;
- 
   std::unordered_map< std::string, unsigned long > dPts;
   for (auto& f : _lsFeatures) {
     if (f->get_class() != BRIDGE) // TODO : all classes
