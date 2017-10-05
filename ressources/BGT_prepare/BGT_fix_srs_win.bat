c7090da206a47e0aa180d03f81561db76b5ed6ab
Tom Commandeur <t.j.f.commandeur@tudelft.nl>
2017-10-05 14:07:36 +0200

    new versions of bgt conversion

--- ressources/BGT_prepare/BGT_fix_srs_win.bat
+++
@@ -1,7 +0,0 @@
-:: rename the files
-move bgt_begroeidterreindeel.gml bgt_begroeidterreindeel_org.gml
-move bgt_onbegroeidterreindeel.gml bgt_onbegroeidterreindeel_org.gml
-:: add the srsDimension to the posList for the kruinlijn
-powershell -Command "(gc 'bgt_begroeidterreindeel_org.gml') -replace '<gml:posList>', '<gml:posList srsDimension=\"2\">' | Out-File bgt_begroeidterreindeel.gml" -Encoding ASCII
-powershell -Command "(gc 'bgt_onbegroeidterreindeel_org.gml') -replace '<gml:posList>', '<gml:posList srsDimension=\"2\">' | Out-File bgt_onbegroeidterreindeel.gml" -Encoding ASCII
-powershell -Command "(gc 'bgt_ondersteunendwegdeel_org.gml') -replace '<gml:posList>', '<gml:posList srsDimension=\"2\">' | Out-File bgt_ondersteunendwegdeel.gml" -Encoding ASCII
