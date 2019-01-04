faccbc64e5dd980d91909b20dd7a7af8521bed68
Tom Commandeur <t.j.f.commandeur@tudelft.nl>
2019-01-04 17:16:33 +0100

    add grid test singular case and horizontal itsct test

--- test/GridTest.cpp
+++ test/GridTest.cpp
@@ -58,6 +58,13 @@ BOOST_AUTO_TEST_CASE(RayLineIntersections) {
   BOOST_CHECK(RayLineIntersection(r, e) == BOUNDARY);
 }
 
+BOOST_AUTO_TEST_CASE(HorizontalRayLineIntersections) {
+  // Ray intersects line in up direction
+  PolyEdge* r = new PolyEdge(0, 5, 5, 5);
+  PolyEdge* e = new PolyEdge(5, 0, 5, 10);
+  BOOST_CHECK(HorizontalRayLineIntersection(r, e) == BOUNDARY);
+}
+
 BOOST_AUTO_TEST_CASE(GridPrepare) {
   std::string wkt = "POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))";
   Polygon2* _p2 = new Polygon2();
@@ -81,4 +88,21 @@ BOOST_AUTO_TEST_CASE(GridCheckPoint) {
 
   BOOST_CHECK(_grid->checkPoint(1, 1) == true);
   BOOST_CHECK(_grid->checkPoint(-1, -1) == false);
+}
+
+BOOST_AUTO_TEST_CASE(GridSingularCell) {
+  std::string wkt = "POLYGON ((0 0, 0 10, 10 10, 10 3, 7.55 3, 7.55 0.2, 10 0.2, 10 0, 0 0))";
+  Polygon2* _p2 = new Polygon2();
+  bg::read_wkt(wkt, *_p2);
+
+  Grid* _grid = new Grid(_p2);
+  _grid->prepare();
+
+  BOOST_CHECK(_grid->checkPoint(7, 1) == true);
+  BOOST_CHECK(_grid->checkPoint(8, 1) == false);
+}
+
+BOOST_AUTO_TEST_CASE(GridPolygonHole) {
+  // create Grid from polygon with hole
+  BOOST_CHECK(false);
 }
