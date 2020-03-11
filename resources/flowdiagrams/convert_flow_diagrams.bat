8465f1a86f3305f43c936ccd47a55c6af8177363
Tom Commandeur <t.j.f.commandeur@tudelft.nl>
2020-03-11 11:34:52 +0100

    docs: update batch export and export all flow diagrams

--- resources/flowdiagrams/convert_flow_diagrams.bat
+++ resources/flowdiagrams/convert_flow_diagrams.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\draw.io\draw.io" --export --format png --border 10 --embed-diagram --output ..\..\docs\images\flows\ .\
+for %%f in ("*.drawio") do "C:\Program Files\draw.io\draw.io" --export --format png --border 10 --embed-diagram --output ..\..\docs\images\flows\%%~nf.png .\%%~nf.drawio
